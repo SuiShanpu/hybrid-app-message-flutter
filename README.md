329d84f08f03822c4fb516dfb9f12e7c0030e0bd
张乾 <qian5z@163.com>
2025-12-10 15:57:48 +0800

    Initial commit: empty project in branch main

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# hybrid-app-message-flutter
+# hybrid_app_message_flutter
+
+A new Flutter project.   
 混合应用开发，交互功能之flutter 端。
---
+++ android/app/src/main/kotlin/com/example/hybrid_app_message_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.hybrid_app_message_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
